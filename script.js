c060582d1dfbe20a3f5937a9d007310266862a25
shahzadaqasimtechnosoft <shahzadaqasim.technosoft@gmail.com>
2021-12-06 14:57:41 +0500

    Added support for keyboard input

--- script.js
+++ script.js
@@ -4,14 +4,26 @@ import {calculate} from "./calculator.js";
 addCalculatorButtons();
 let calculatorScreen = document.getElementById('screen');
 
+function reactToInputChanged(inputText) {
+    if (inputText === 'C' || inputText === 'c') {
+        calculatorScreen.innerText = '';
+    } else if (inputText === '=') {
+        calculatorScreen.innerText = calculate(calculatorScreen.innerText);
+    } else {
+        calculatorScreen.innerText = calculatorScreen.innerText + inputText;
+    }
+}
+
 document.querySelectorAll('.calculator-button').forEach(item => {
     item.addEventListener('click', event => {
-        if (event.target.innerText === 'C') {
-            calculatorScreen.innerText = '';
-        } else if (event.target.innerText === '=') {
-            calculatorScreen.innerText = calculate(calculatorScreen.innerText);
-        } else {
-            calculatorScreen.innerText = calculatorScreen.innerText + event.target.innerText;
-        }
+        reactToInputChanged(event.target.innerText);
     });
+});
+
+let permissibleKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '/', '*', '=', 'c'];
+
+document.addEventListener('keydown', event => {
+    if (permissibleKeys.includes(event.key)) {
+        reactToInputChanged(event.key);
+    }
 });
