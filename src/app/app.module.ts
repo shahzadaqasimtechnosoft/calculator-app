b56e03e672fedfd785336ed2efbf4163251cf7e9
shahzadaqasimtechnosoft <shahzadaqasim.technosoft@gmail.com>
2021-12-10 12:13:46 +0500

    Renamed calculator-body to calculator-button-area

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
-import { CalculatorBodyComponent } from './calculator/calculator-body/calculator-body.component';
+import { CalculatorButtonAreaComponent } from './calculator/calculator-button-area/calculator-button-area.component';
 import { CalculatorScreenComponent } from './calculator/calculator-screen/calculator-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalculatorComponent,
-    CalculatorBodyComponent,
+    CalculatorButtonAreaComponent,
     CalculatorScreenComponent
   ],
   imports: [
---
+++ src/app/calculator/calculator-button-area/calculator-button-area.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator-button-area',
+  templateUrl: './calculator-button-area.component.html',
+  styleUrls: ['./calculator-button-area.component.css']
+})
+export class CalculatorButtonAreaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
