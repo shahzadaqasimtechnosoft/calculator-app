aad6963bbf58b19f54b503becfcd68d4943877c0
shahzadaqasimtechnosoft <shahzadaqasim.technosoft@gmail.com>
2021-12-10 16:21:50 +0500

    Added ButtonAreaService to ButtonArea component

--- src/app/calculator/calculator-button-area/calculator-button-area.component.ts
+++ src/app/calculator/calculator-button-area/calculator-button-area.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CalculatorButtonAreaService} from "./calculator-button-area.service";
+import {CalculatorButton} from "./calculator-button/calculator-button.model";
 
 @Component({
   selector: 'app-calculator-button-area',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator-button-area.component.css']
 })
 export class CalculatorButtonAreaComponent implements OnInit {
+  buttons!: CalculatorButton[];
 
-  constructor() { }
+  constructor(private buttonService: CalculatorButtonAreaService) { }
 
   ngOnInit(): void {
+    this.buttons = this.buttonService.getButtons();
   }
-
 }
