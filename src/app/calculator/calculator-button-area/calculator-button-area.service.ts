1f34b3f71357f2d47cf57c1858af957ebc7270ec
shahzadaqasimtechnosoft <shahzadaqasim.technosoft@gmail.com>
2021-12-10 17:53:16 +0500

    Made calculator workable

--- src/app/calculator/calculator-button-area/calculator-button-area.service.ts
+++ src/app/calculator/calculator-button-area/calculator-button-area.service.ts
@@ -1,9 +1,11 @@
 import {Injectable} from "@angular/core";
-import {CalculatorButton} from "./calculator-button/calculator-button.model";
 import {Subject} from "rxjs";
+
+import {CalculatorButton} from "./calculator-button/calculator-button.model";
 
 @Injectable({providedIn: 'root'})
 export class CalculatorButtonAreaService {
+  private input: string = '';
   inputMaker = new Subject<string>();
 
   private buttons: CalculatorButton[] = [
@@ -25,11 +27,20 @@ export class CalculatorButtonAreaService {
     new CalculatorButton('/', 'operation')
   ];
 
+  constructor() {}
+
   getButtons(): CalculatorButton[] {
     return this.buttons;
   }
 
   addToInput(value: string) {
-    this.inputMaker.next(value);
+    if (value === '=') {
+      this.input = eval(this.input);
+    } else if (value === 'C') {
+      this.input = '';
+    } else {
+      this.input = this.input + value;
+    }
+    this.inputMaker.next(this.input);
   }
 }
--- src/app/calculator/calculator-screen/calculator-screen.component.ts
+++ src/app/calculator/calculator-screen/calculator-screen.component.ts
@@ -7,14 +7,14 @@ import {CalculatorButtonAreaService} from "../calculator-button-area/calculator-
   styleUrls: ['./calculator-screen.component.css']
 })
 export class CalculatorScreenComponent implements OnInit, OnDestroy {
-  input: string = '';
+  input!: string;
 
   constructor(private buttonService: CalculatorButtonAreaService) {}
 
   ngOnInit(): void {
     this.buttonService.inputMaker
       .subscribe((value:string) => {
-        this.input = this.input + value;
+        this.input = value;
       });
   }
 
