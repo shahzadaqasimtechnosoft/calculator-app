0e2e3d1e0bb0824cc76fefe8d5bb5d8964bd3d41
shahzadaqasimtechnosoft <shahzadaqasim.technosoft@gmail.com>
2021-12-10 12:55:36 +0500

    Added styles to body and button area + added calculator button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { CalculatorButtonAreaComponent } from './calculator/calculator-button-area/calculator-button-area.component';
 import { CalculatorScreenComponent } from './calculator/calculator-screen/calculator-screen.component';
+import { CalculatorButtonComponent } from './calculator/calculator-button-area/calculator-button/calculator-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalculatorComponent,
     CalculatorButtonAreaComponent,
-    CalculatorScreenComponent
+    CalculatorScreenComponent,
+    CalculatorButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/calculator/calculator-button-area/calculator-button/calculator-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator-button',
+  templateUrl: './calculator-button.component.html',
+  styleUrls: ['./calculator-button.component.css']
+})
+export class CalculatorButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
